b99c9fd73980f61a9cb2afb28e2b8531aa57e442
22H51AO529 <kailasarakshitha@gmail.com>
2024-11-20 17:50:02 +0530

    fifth commit

--- Prime.java
+++ Prime.java
@@ -21,5 +21,6 @@ public class Prime
         System.out.println("prime");
             System.out.println("prime number");
         System.out.println("hello");
+        System.out.println("job prime");
     }
 }
